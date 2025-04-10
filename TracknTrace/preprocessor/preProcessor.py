2464548a7f2f3383f9f361382012adc259ee57c2
Jeroen van 't Ende <jeroenende@hotmail.com>
2025-04-10 13:29:42 +0200

    Update preProcessor.py
    different time formatting default for csv

--- TracknTrace/preprocessor/preProcessor.py
+++ TracknTrace/preprocessor/preProcessor.py
@@ -804,7 +804,7 @@ def ProcessData():
     if dataformat == "Excel":
         data = pd.read_excel(config["preprocessing"]["Filename"], sheet_name = "Data", header=[0,1])
         data["DateTime"] = data[["Date","Time"]].apply(lambda x: str(x[0])[:10]+" "+str(x[1]), axis=1)
-        data["DateTime"] = pd.to_datetime(data.DateTime, format="%Y-%m-%d %H:%M:%S")
+        data["DateTime"] = pd.to_datetime(data.DateTime, format="%d-%m-%Y %H:%M:%S")
         data["Datetime"] = data.loc[:, "DateTime"]#data[["DateTime"]]
         data = data.drop(["Date","Time"], axis=1)
         data = data.set_index("DateTime")
